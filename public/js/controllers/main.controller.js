48551d0120a8229ceb03c291387bcae62a237b7c
Aaron Schwartz <aschwartz628@gmail.com>
2016-12-01 20:11:41 -0500

    added ability to search name by uppercase or lowercase

--- public/js/controllers/main.controller.js
+++ public/js/controllers/main.controller.js
@@ -6,16 +6,22 @@
 
   function MainController($scope, CoinSummaryService){
     $scope.coins = CoinSummaryService.get();
-
+    $scope.searchCoins = searchCoins;
     $scope.$watch(function(){
       return CoinSummaryService.get();
     }, function(){
         $scope.coins = CoinSummaryService.get();
       });
+
+      function searchCoins(coinName){
+        var coin = _.find($scope.coins, function(o){
+        return o.name.toUpperCase() === coinName.toUpperCase();
+      });
+      console.log(coin);
+    }
+    console.log(searchCoins());
   }
 
-    _.find(coins, function(o){
-      return o.name === "Bitcoin";
-    });
+
 
 }());
