e545a968f676f2afbad5e99b81de7ff1d1fe0aa8
Aaron Schwartz <aschwartz628@gmail.com>
2016-12-02 23:33:29 -0500

    added more coin charts

--- public/js/controllers/main.controller.js
+++ public/js/controllers/main.controller.js
@@ -17,9 +17,28 @@
         var coin = _.find($scope.coins, function(o){
         return o.name.toUpperCase() === coinName.toUpperCase();
       });
-      console.log(coin);
+      $scope.selected = coin;
     }
     console.log(searchCoins());
+
+    $scope.widgetLookup = {
+      "Bitcoin": "https://dc-charts.com/fw/coinbase_btc_usd_24hours_3.png",
+      "Litecoin": "https://dc-charts.com/fw/bitfinex_ltc_btc_24hours_3.png",
+      "Dash": "https://dc-charts.com/fw/poloniex_dash_btc_24hours_3.png",
+      "Dogecoin": "https://dc-charts.com/fw/poloniex_doge_btc_24hours_3.png",
+      "Earthcoin": "https://dc-charts.com/fw/btc38_eac_btc_24hours_3.png",
+      "Ethereum": " https://dc-charts.com/fw/poloniex_eth_btc_24hours_3.png",
+      "Monero": "https://dc-charts.com/fw/hitbtc_xmr_btc_24hours_3.png",
+      "Namecoin": "https://dc-charts.com/fw/btce_nmc_btc_24hours_3.png",
+      "Novacoin": "https://dc-charts.com/fw/btce_nvc_btc_24hours_3.png",
+      "Nxt": "https://dc-charts.com/fw/hitbtc_nxt_btc_24hours_3.png",
+      "Peercoin": "https://dc-charts.com/fw/btce_ppc_btc_24hours_3.png",
+      "Primecoin": "https://dc-charts.com/fw/btc38_xpm_cny_24hours_3.png",
+      "Ripple": "https://dc-charts.com/fw/poloniex_xrp_btc_24hours_3.png",
+      "Stellar": "https://dc-charts.com/fw/poloniex_str_btc_24hours_3.png"
+
+    };
+
   }
 
 
