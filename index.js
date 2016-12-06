32c575d115e12dbb6aa65ed227c57503bc876d3a
Aaron Schwartz <aschwartz628@gmail.com>
2016-12-05 20:23:30 -0500

    added mongoose, body parser npm packages, added backend skeleton and adjusted index.js

--- index.js
+++ index.js
@@ -1,6 +1,13 @@
 var express = require('express');
 var server = express();
 
+var coinRouter = require('./server/routers/coin.router.js');
+
+var mongoose = require('mongoose');
+
+var mongoURI = process.env.MONGOURI || require('./config.js').mongoURI;
+mongoose.connect(mongoURI);
+
 var port = process.env.PORT || 8080;
 
 server.use(express.static(__dirname + '/public'));
@@ -8,6 +15,8 @@ server.use(express.static(__dirname + '/public'));
 server.get('/', function(req, res){
   res.sendFile('public/html/index.html', {root:__dirname});
 });
+
+server.use(coinRouter);
 
 server.listen(port, function(){
   console.log('Now listening on port...', port);
